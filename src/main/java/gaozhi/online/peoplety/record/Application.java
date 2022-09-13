6f99676aca87ecb64978623ae21cbcd05a46b11b
LFC_dorm <1240902193@qq.com>
2022-09-13 23:19:36 +0800

    删除一些没用的代码

--- src/main/java/gaozhi/online/peoplety/record/Application.java
+++ src/main/java/gaozhi/online/peoplety/record/Application.java
@@ -2,13 +2,9 @@ package gaozhi.online.peoplety.record;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author LiFucheng
@@ -19,15 +15,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 @EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"gaozhi.online.peoplety.record.service.feign"})
-@ComponentScan(basePackageClasses = {gaozhi.online.base.ScanClass.class,gaozhi.online.peoplety.ScanClass.class, Application.class})
+//@EnableFeignClients(basePackages = {"gaozhi.online.peoplety.record.service.feign"})
+@ComponentScan(basePackageClasses = {gaozhi.online.base.ScanClass.class, gaozhi.online.peoplety.ScanClass.class, Application.class})
 public class Application {
-    //负载均衡
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
